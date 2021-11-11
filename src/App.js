30898e179a098cf2cc28220c604a3985926a27ed
mariia-ivanski <mariia.ivanski@gmail.com>
2021-11-11 22:44:15 +0200

    set up dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Mariia Ivanski, and it is open-sourced on
+            GitHub
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
