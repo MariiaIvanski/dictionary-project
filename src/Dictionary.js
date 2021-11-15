d274cf52f2779f27b18e1dac99914b7e2bd84be0
mariia-ivanski <mariia.ivanski@gmail.com>
2021-11-15 15:59:17 +0200

    update Dictionary design

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -35,7 +35,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
-          <h2>What word do you want to look up?</h2>
+          <h4>What word do you want to look up?</h4>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
